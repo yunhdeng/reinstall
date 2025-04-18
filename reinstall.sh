e6dd1eb12a260203fe6090161c3b6c77cf9be3ad
yunhdeng <89250118+yunhdeng@users.noreply.github.com>
2025-04-18 11:37:21 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,9 +3,8 @@
 # shellcheck disable=SC2086
 
 set -eE
-confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
-confhome_cn=https://cnb.cool/bin456789/reinstall/-/git/raw/main
-# confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
+confhome=https://raw.githubusercontent.com/yunhdeng/reinstall/main
+confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/yunhdeng/reinstall/main
 
 # 默认密码
 DEFAULT_PASSWORD=123@@@
